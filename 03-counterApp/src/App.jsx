37410e928fd17396eea7492368a245d5606b0c78
Prithvi-Pratap <pratapprithvi768@gmail.com>
2024-02-12 23:08:29 +0530

    hooks in react

---
+++ 03-counterApp/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,SetCounter] =useState(15)//can give any default value
+
+  // let counter=15;
+     
+  const incCounter=()=>{
+    counter=counter+1;
+    SetCounter(counter);//set the updated state
+    console.log('clicked.',counter)
+  }
+
+  const decCounter=()=>{
+    // counter=counter-1;
+    SetCounter(counter-1);
+    console.log("clicked.",counter)
+  }
+
+  return (
+    <>
+     <h1>Chai aur react</h1>
+     <h2>Counter value {counter}</h2>
+
+     <button onClick={incCounter} >Add Value {counter}</button>
+     <br />
+     <button onClick={decCounter}>remove value {counter}</button>
+    </>
+  )
+}
+
+export default App
